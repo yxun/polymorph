accfd21fbcd4a78d2b75737e71f5b05eb6c017a5
yxun <yuanlin.yxu@gmail.com>
2021-01-15 23:30:55 -0500

    Add Block message

---
+++ foundation/src/main/java/message/Block.java
@@ -0,0 +1,36 @@
+package message;
+
+import java.util.Date;
+
+import util.StringUtil;
+
+public class Block {
+    public String hash;
+    public String previousHash;
+    private String data;
+    private long timeStamp;   // as a number of milliseconds since 1/1/1970
+    private int nonce;
+
+    public Block(String data, String previousHash) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+        this.hash = CalculateHash(); // Make sure we call this after we set the other fields
+    }
+
+    public String CalculateHash() {
+        String calculatedhash = StringUtil.applySHA256(
+            previousHash + Long.toString(timeStamp) + data + Integer.toString(nonce)
+        );
+        return calculatedhash;
+    }
+
+    public void Init(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while (!hash.substring(0, difficulty).equals(target)) {
+            nonce++;
+            hash = CalculateHash();  // calculate hash until it starts with difficulty number of 0s
+        }
+        System.out.println("Initial block: " + hash);
+    }
+}
---
+++ foundation/src/test/BlockTest.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import message.Block;
+
+public class BlockTest {
+
+    public static ArrayList<Block> chain = new ArrayList<Block>();
+    public static int difficulty = 5;
+
+    public static void main(String[] args) {
+        chain.add(new Block("This is the first block", "0"));
+        chain.get(0).Init(difficulty);
+        chain.add(new Block("This is the second block", chain.get(chain.size()-1).hash));
+        chain.get(1).Init(difficulty);
+        chain.add(new Block("This is the third block", chain.get(chain.size()-1).hash));
+        chain.get(2).Init(difficulty);
+
+        System.out.println("Check chain is valid: " + isChainValid());
+        for (Block b : chain) {
+            System.out.println("hash: "+ b.hash + "  previousHash: " + b.previousHash);
+        }
+    }
+
+    public static Boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+        String hashTarget = new String(new char[difficulty]).replace('\0', '0');
+
+        for (int i = 1; i < chain.size(); i++) {
+            currentBlock = chain.get(i);
+            previousBlock = chain.get(i-1);
+            // compare registered hash and calculated hash
+            if (!currentBlock.hash.equals(currentBlock.CalculateHash())) {
+                System.out.println("Current Hashes not equal");
+                return false;
+            }
+            // compare previous hash and registered previous hash
+            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+                System.out.println("Previous Hashes not equal");
+                return false;
+            }
+            // check if hash is initialized
+            if (!currentBlock.hash.substring(0, difficulty).equals(hashTarget)) {
+                System.out.println("This block hasn't been initialized");
+                return false;
+            }
+        }
+        return true;
+    }
+    
+}
